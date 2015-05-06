774387b8f4f194d3535bd5acb02dde5bafee0dae
Dylan Sturgeon <dylansturg@gmail.com>
2015-05-06 12:27:08 -0400

    initial Images project

---
+++ Images/Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Images
+//
+//  Created by Dylan Sturgeon on 5/6/15.
+//  Copyright (c) 2015 dylansturg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Images/Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Images
+//
+//  Created by Dylan Sturgeon on 5/6/15.
+//  Copyright (c) 2015 dylansturg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
